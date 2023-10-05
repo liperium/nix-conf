f92e0ca11dd529b87fa9b46f7232474d6e4438fe
Mattys Gervais <mattysgervais@gmail.com>
2023-10-05 07:58:49 -0400

    conservationmode works

--- hosts/lenovo/conservationmode.nix
+++ hosts/lenovo/conservationmode.nix
@@ -8,11 +8,21 @@ let
         cat $conservationmode_file
     elif [ "$1" = "on" ]
     then 
+        if [[ $EUID -ne 0 ]]; then
+            echo "This script must be run as root. Re-executing with sudo..."
+            sudo "$0" "$@"   # This will re-run the script as root
+            exit $?          # Exit with the status returned by the sudo command
+        fi
         echo 1 | sudo tee $conservationmode_file
     elif [ "$1" = "off" ]
     then 
+        if [[ $EUID -ne 0 ]]; then
+            echo "This script must be run as root. Re-executing with sudo..."
+            sudo "$0" "$@"   # This will re-run the script as root
+            exit $?          # Exit with the status returned by the sudo command
+        fi
         echo 0 | sudo tee $conservationmode_file
-    elif [ "$1" = "-h" ] || [ "$1" = "-help" ]
+    elif [ "$1" = "-h" ] || [ "$1" = "--help" ]
     then
         printf " -h/-help : help \n on : turns conservation_mode on \n off : turns conservation_mode off \n status : returns on/off (1/0)\n"
     fi
@@ -25,7 +35,7 @@ in {
     extraRules = [{
       commands = [
         {
-          command = "${conservationmode}";
+          command = "/run/current-system/sw/bin/conservationmode";
           options = [ "NOPASSWD" ];
         }
       ];
