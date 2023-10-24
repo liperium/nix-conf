f7bcd83ebe3c7046a56358bb4a35d900b634f555
Mattys Gervais <mattysgervais@gmail.com>
2023-10-24 15:38:39 -0400

    upgraded and remove after 5 instances

--- modules/base.nix
+++ modules/base.nix
@@ -6,7 +6,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 30d";
+    options = "--delete-older-than +5";
   };
   nix.settings.auto-optimise-store = true;
 
