a2162bb85e5c7235f7e409d2dec7645d5e111a66
Mattys Gervais <mattysgervais@gmail.com>
2023-11-01 12:02:14 -0400

    no make deps

--- modules/desktop/godot-mono.nix
+++ modules/desktop/godot-mono.nix
@@ -1,10 +1,9 @@
 { config, pkgs, lib, ... }:
 let
   godot4-mono = pkgs.callPackage ./godot4-mono/default.nix { };
-  godot4-make-deps = pkgs.callPackage ./godot4-mono/make-deps.nix { };
 in
 {
   users.users.liperium = {
-    packages = with pkgs; [ steam-run dotnet-sdk jetbrains.rider godot4-mono godot4-make-deps ];
+    packages = with pkgs; [ steam-run dotnet-sdk jetbrains.rider godot4-mono ];
   };
 }
