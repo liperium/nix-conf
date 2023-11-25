01ddb4bde692b142738fde1416513f1e67fbb7d7
Mattys Gervais <mattysgervais@gmail.com>
2023-11-25 16:55:12 -0500

    nix quick update dir fix

--- modules/nix-quick-update/default.nix
+++ modules/nix-quick-update/default.nix
@@ -2,7 +2,7 @@
 let
   nix-quick-update = pkgs.writeShellScriptBin "nix-quick-update" ''
     #!/bin/sh
-    git pull && sudo nixos-rebuild switch --flake /home/liperium/nix-conf
+    cd /home/liperium/nix-conf && git pull && sudo nixos-rebuild switch --flake
   '';
 in {
   environment.systemPackages = [ nix-quick-update ];
