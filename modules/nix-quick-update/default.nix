4ca164a5677127e4a2b08e4ea02a968f6d9ebd92
Mattys Gervais <mattysgervais@gmail.com>
2023-11-26 22:18:46 -0500

    workspaces

--- modules/nix-quick-update/default.nix
+++ modules/nix-quick-update/default.nix
@@ -2,7 +2,9 @@
 let
   nix-quick-update = pkgs.writeShellScriptBin "nix-quick-update" ''
     #!/bin/sh
-    cd /home/liperium/nix-conf && git pull && sudo nixos-rebuild switch --flake
+    cd /home/liperium/nix-conf; 
+    git pull; 
+    sudo nixos-rebuild switch --flake /home/liperium/nix-conf;
   '';
 in {
   environment.systemPackages = [ nix-quick-update ];
