d33c125afb2554f11302db649c2147079f1ecad8
Mattys Gervais <mattysgervais@gmail.com>
2023-11-30 15:08:58 -0500

    polkit autostart

--- modules/desktop/environment/hyprland/default.nix
+++ modules/desktop/environment/hyprland/default.nix
@@ -26,6 +26,7 @@
     kitty
     #gnome.gnome-keyring # secrets manager
     libsecret
+    polkit_gnome
     
     waybar # Top status bar
     wlogout
@@ -45,6 +46,7 @@
     gnome.eog # Image viewer
     #hyprpicker # Color picker, doesn't work?
   ];
+  security.polkit.enable = true;
   programs.thunar.plugins = with pkgs.xfce; [
     thunar-archive-plugin
     thunar-volman
@@ -77,5 +79,19 @@
     jack.enable = true;
   };
 
-
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+    };
+  };
 }
