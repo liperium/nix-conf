072e73974398072b4559e59ac333c7789dbfbafd
Mattys Gervais <mattysgervais@gmail.com>
2023-12-23 19:12:05 -0500

    nch is

--- hosts/atlas/default.nix
+++ hosts/atlas/default.nix
@@ -3,10 +3,15 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false; # Or can't install boot loader
-  
+
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./modules.nix
   ];
   networking.hostName = "atlas";
+  
+  services.openssh = {
+    enable = true;
+    ports = [5252];
+  };
 }
