266375fa6c392205f94eebde2cef9d80f1f318f2
Mattys Gervais <mattysgervais@gmail.com>
2023-12-25 18:25:55 -0500

    adr

--- hosts/atlas/default.nix
+++ hosts/atlas/default.nix
@@ -22,10 +22,10 @@
   networking = {
     interfaces.eno1.useDHCP = false;
     interfaces.eno1.ipv4.addresses = [{
-      address = "192.168.1.10";
+      address = "192.168.0.20";
       prefixLength = 24;
     }];
-    defaultGateway = "192.168.1.1";
+    defaultGateway = "192.168.0.1";
     nameservers = [ "192.168.0.10" ];
   };
 
