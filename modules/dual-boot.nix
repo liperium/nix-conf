21872ce82dcad827faed099c70ea373235e55de6
Mattys Gervais <48138387+liperium@users.noreply.github.com>
2024-01-22 10:02:43 -0500

    systemd boot

--- modules/dual-boot.nix
+++ modules/dual-boot.nix
@@ -1,16 +1,7 @@
 { config, pkgs, lib, ... }: {
   # Bootloader.
   boot.supportedFilesystems = [ "ntfs" ];
-  #boot.loader.systemd-boot.enable = true;
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    grub.enable = true;
-    grub.devices = [ "nodev" ];
-    grub.efiSupport = true;
-    grub.useOSProber = true;
-  };
-  environment.systemPackages = with pkgs;
-    [
-      os-prober # Probes for windows for grub
-    ];
+  
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 }
