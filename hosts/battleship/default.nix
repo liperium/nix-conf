82f43802fb0f8b4bf2e7547d61f8fea1ac98c894
Mattys Gervais <mattysgervais@gmail.com>
2024-02-25 22:41:58 -0500

    added nextcloud top right

--- hosts/battleship/default.nix
+++ hosts/battleship/default.nix
@@ -18,7 +18,7 @@
     os-prober # Probes for windows for grub
   ];
 
-  #services.xserver.displayManager.defaultSession = "plasmawayland";
+  services.xserver.displayManager.defaultSession = "hyprland";
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [
