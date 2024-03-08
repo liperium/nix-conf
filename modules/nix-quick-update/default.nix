a948dcec27a7f3b5cc1e11015191a3a550a05395
Mattys Gervais <mattysgervais@gmail.com>
2024-03-08 17:50:10 -0500

    gtkrc symlink remove on update

--- modules/nix-quick-update/default.nix
+++ modules/nix-quick-update/default.nix
@@ -2,6 +2,7 @@
 let
   nix-quick-update = pkgs.writeShellScriptBin "nix-quick-update" ''
     #!/bin/sh
+    rm /home/liperium/.gtkrc-2.0
     cd /home/liperium/nix-conf; 
     git pull; 
     sudo nixos-rebuild switch --flake /home/liperium/nix-conf;
