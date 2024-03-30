5e3a46a6db6cfe69d1ba939d3aa5ea5124d515c5
Mattys Gervais <mattysgervais@gmail.com>
2024-03-29 22:58:52 -0400

    gnome auth agent removed

--- modules/desktop/environment/hyprland/default.nix
+++ modules/desktop/environment/hyprland/default.nix
@@ -39,6 +39,8 @@
     #polkit_gnome
     kdePackages.polkit-qt-1
     kdePackages.polkit-kde-agent-1
+    kdePackages.kwallet
+    kdePackages.kwallet-pam
     
     waybar # Top status bar # Home
     wlogout # Home
@@ -57,7 +59,7 @@
     grimblast # Screenshots
     #gnome.nautilus # File explorer
     kdePackages.dolphin
-    gnome.eog # Image viewer
+    #gnome.eog # Image viewer
 
     #xwaylandvideobridge #not working... works for 5s
     #kdeconnect #not working???
@@ -79,7 +81,7 @@
   # Portal to make it easy
   xdg.portal.enable = true;
   xdg.portal.extraPortals = with pkgs; [ 
-    xdg-desktop-portal-gtk
+    kdePackages.xdg-desktop-portal-kde
     #xdg-desktop-portal-hyprland already applied?
   ];
 
@@ -95,18 +97,31 @@
   };
 
   systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-          Type = "simple";
-          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
-    };
+    #user.services.polkit-gnome-authentication-agent-1 = {
+    #  description = "polkit-gnome-authentication-agent-1";
+    #  wantedBy = [ "graphical-session.target" ];
+    #  wants = [ "graphical-session.target" ];
+    #  after = [ "graphical-session.target" ];
+    #  serviceConfig = {
+    #      Type = "simple";
+    #      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+    #      Restart = "on-failure";
+    #      RestartSec = 1;
+    #      TimeoutStopSec = 10;
+    #    };
+    #};
+    #user.services.kwallet-pam-init-1 = {
+    #  description = "Launches kwallet";
+    #  wantedBy = [ "graphical-session.target" ];
+    #  wants = [ "graphical-session.target" ];
+    #  after = [ "graphical-session.target" ];
+    #  serviceConfig = {
+    #      Type = "simple";
+    #      ExecStart = "${pkgs.kdePackages.kwallet-pam}/libexec/pam_kwallet_init";
+    #      Restart = "on-failure";
+    #      RestartSec = 1;
+    #      TimeoutStopSec = 10;
+    #    };    
+    #};
   };
 }
