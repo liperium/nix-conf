b07332b3b863832d0ae01a060b56bc7a9ab72695
Mattys Gervais <mattysgervais@gmail.com>
2024-04-17 22:51:19 -0400

    fixes

--- home/home.nix
+++ home/home.nix
@@ -102,18 +102,11 @@
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+  home.file.".config/hypr/auth.conf".text = ''
+    exec-once = ${pkgs.libsForQt5.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1
+    exec-once = ${pkgs.libsForQt5.kwallet-pam}/libexec/pam_kwallet_init
+  '';
 
   # You can also manage environment variables but you will have to manually
   # source
