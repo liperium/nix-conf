258bb2984265dfff805d9c8a8bb1639c7e656768
Mattys Gervais <48138387+liperium@users.noreply.github.com>
2024-05-02 23:06:12 -0400

    fix sude

--- modules/nix-quick-update/default.nix
+++ modules/nix-quick-update/default.nix
@@ -4,8 +4,8 @@ let
     #!/bin/sh
     rm /home/liperium/.gtkrc-2.0
     cd /home/liperium/nix-conf; 
-    sudo -u liperium bash -c 'git pull'; 
-    sudo nh os switch /home/liperium/nix-conf;
+    git pull; 
+    nh os switch ./;
   '';
 in
 {
