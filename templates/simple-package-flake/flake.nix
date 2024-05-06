9fa8b19e891381497ec499668ac8b96f0eed0cb1
Mattys Gervais <48138387+liperium@users.noreply.github.com>
2024-05-05 21:40:20 -0400

    base flake is better

--- templates/simple-package-flake/flake.nix
+++ templates/simple-package-flake/flake.nix
@@ -1,13 +1,19 @@
 {
-  description = "Basic dev flake";
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
-      buildInputs = [
-        # zsh
-      ];
-    };
-  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ 
+            #pkgs.bashInteractive 
+          ];
+        };
+      });
 }
+#echo "use flake" >> .envrc && direnv allow
