0ccbbffa15197f1b2272e00d8d68471508630035
Mattys Gervais <mattysgervais@gmail.com>
2024-05-10 13:14:00 -0400

    added comment to start network

--- modules/desktop/virt.nix
+++ modules/desktop/virt.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, dconf, ... }:
 
 {
+  # sudo virsh net-start default
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
   environment.systemPackages = with pkgs; [
