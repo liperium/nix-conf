126b529ea13f835698665937f6f2525ead5db409
Mattys Gervais <mattysgervais@gmail.com>
2024-05-27 09:14:05 -0400

    update removed rm

--- modules/nix-quick-update/default.nix
+++ modules/nix-quick-update/default.nix
@@ -2,7 +2,6 @@
 let
   nix-quick-update = pkgs.writeShellScriptBin "nix-quick-update" ''
     #!/bin/sh
-    rm /home/liperium/.gtkrc-2.0
     cd /home/liperium/nix-conf; 
     git pull; 
     nh os switch ./;
