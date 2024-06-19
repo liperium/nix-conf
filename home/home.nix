9244d672f945e04548e0b470e02b31f192ae034b
Mattys Gervais <mattysgervais@gmail.com>
2024-06-19 15:45:40 -0400

    yes yes

--- home/home.nix
+++ home/home.nix
@@ -10,22 +10,14 @@
     ./helix
     ./zellij
   ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  
   home.username = "liperium";
   home.homeDirectory = "/home/liperium";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     fastfetch
     dconf
@@ -90,20 +82,6 @@
       nix-direnv.enable = true;
     };
   };
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/liperium/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
@@ -220,7 +198,4 @@
       gtk-application-prefer-dark-theme = 1;
     };
   };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
