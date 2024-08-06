62cfad290413f3a5d6ca241b84bc67888abac210
Mattys Gervais <mattysgervais@gmail.com>
2024-08-05 21:48:27 -0400

    revert teamviewer shit

--- hosts/battleship/default.nix
+++ hosts/battleship/default.nix
@@ -12,10 +12,8 @@
   ];
 
   networking.hostName = "battleship"; # Define your hostname.
-  services.teamviewer.enable = true;
 
   environment.systemPackages = with pkgs; [
-    teamviewer
     nvtopPackages.amd
     path-of-building
     os-prober # Probes for windows for grub
