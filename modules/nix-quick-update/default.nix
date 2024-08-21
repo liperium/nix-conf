dea25e2760c65fae1343e050b1013ab79841a0da
Mattys Gervais <mattysgervais@gmail.com>
2024-08-21 18:23:35 -0400

    rm gtk2.0

--- modules/nix-quick-update/default.nix
+++ modules/nix-quick-update/default.nix
@@ -7,7 +7,6 @@ let
   nix-quick-update = pkgs.writeShellScriptBin "nix-quick-update" ''
     #!/bin/sh
     cd /home/liperium/nix-conf; 
-    rm /home/liperium/.gtkrc-2.0
     git pull; 
     nh os switch ./;
   '';
