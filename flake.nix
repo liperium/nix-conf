eb8e85989b76006502f539c33b4c207ed600adde
Mattys Gervais <mattysgervais@gmail.com>
2024-09-17 10:21:42 -0400

    update

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
           {
             nix.settings = {
               substituters = [ "https://cosmic.cachix.org/" ];
-              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02s" ];
+              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
             };
           }
           nixos-cosmic.nixosModules.default
