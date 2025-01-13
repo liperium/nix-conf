6ef1fe9bb8d52ca89c39ed08f62240e028604c5c
Mattys Gervais <mattysgervais@gmail.com>
2025-01-12 22:52:51 -0500

    caddy new services + greetd/regreet

--- hosts/battleship/default.nix
+++ hosts/battleship/default.nix
@@ -6,23 +6,39 @@
     ./modules.nix
   ];
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-  networking.hostName = "battleship"; 
+  networking.hostName = "battleship";
 
   environment.systemPackages = with pkgs; [
     nvtopPackages.amd
     os-prober # Probes for windows for grub
+    greetd.regreet
   ];
-  services.fstrim.enable = true;
 
-  services.xserver = {
-    enable = true; # services.xserver.enable = true
-    displayManager = {
-      gdm = {
-        enable = true; # services.xserver.displayManager.gdm.enable = true
-        wayland = true; # services.xserver.displayManager.gdm.wayland = true
+  # Greetd start
+  environment.etc = {
+    greetd-regreet-hyprland = {
+      text = ''
+        exec-once = ${pkgs.greetd.regreet}/bin/regreet; hyprctl dispatch exit
+        misc {
+            disable_hyprland_logo = true
+            disable_splash_rendering = true
+            disable_hyprland_qtutils_check = true
+        }
+      '';
+      mode = "4444";
+    };
+  };
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland --config /etc/greetd-regreet-hyprland";
+        user = "greeter";
       };
     };
   };
+  programs.regreet.enable = true;
+  # Greetd end
 
   services.openssh = {
     enable = true;
--- hosts/shuttle/services.nix
+++ hosts/shuttle/services.nix
@@ -20,6 +20,11 @@
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = { };
+
+      http = {
+        use_x_forwarded_for = true;
+        trusted_proxies = [ "192.168.0.10" "127.0.0.1" ];
+      };
     };
   };
   services.matter-server.enable = true; # Matter wattage plugs
