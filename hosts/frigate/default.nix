92b7253d994f7f90b14c119d64f05ae6785a0264
Mattys Gervais <mattysgervais@gmail.com>
2025-03-10 23:11:28 -0400

    polkit stuff for laptop!

--- hosts/frigate/default.nix
+++ hosts/frigate/default.nix
@@ -75,9 +75,27 @@ in
   environment.systemPackages = with pkgs;
     [
       fprintd
+      polkit_gnome
+      bitwarden-desktop
       # Hyprpanel
     ];
+  #programs.bitwarden.enable = true;
 
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
   # Bluetooth
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = false;
