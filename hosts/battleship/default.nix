d74057f0c0086b5e52743120bc58e51cfa7f2930
Mattys Gervais <mattysgervais@gmail.com>
2025-03-10 23:16:54 -0400

    battleship polish

--- hosts/battleship/default.nix
+++ hosts/battleship/default.nix
@@ -21,7 +21,7 @@ in
     (catppuccin-grub.override {
       flavor = "mocha";
     })
-    polkit_gnome
+    hyprpolkitagent
   ];
 
   boot = {
@@ -47,22 +47,6 @@ in
       grub.theme = "${pkgs.catppuccin-grub}";
     };
   };
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
-
 
   # Greetd start
   environment.etc = {
