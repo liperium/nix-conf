f68e8841759218f8b59f57b1d47a7f7cd9dc419c
Mattys Gervais <mattysgervais@gmail.com>
2025-04-04 11:31:15 -0400

    add gnome as fallback

--- hosts/frigate/default.nix
+++ hosts/frigate/default.nix
@@ -58,10 +58,11 @@ in
   # GDM
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   # Autologin
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "liperium";
+  services.displayManager.defaultSession = "hyprland";
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
