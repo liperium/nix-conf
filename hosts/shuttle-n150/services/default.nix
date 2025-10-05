4afbfa68e26d94456aa45363b14a9ac299e996e0
Mattys Gervais <mattysgervais@gmail.com>
2025-10-05 02:16:27 -0400

    jellyfin transcoding vaapi

--- hosts/shuttle-n150/services/default.nix
+++ hosts/shuttle-n150/services/default.nix
@@ -57,6 +57,23 @@
   services.prowlarr.enable = true; # 9696
   services.sonarr.enable = true; # 8989
   services.jellyfin.enable = true; # 8096
+  systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "iHD";
+  environment.systemPackages = [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
+  ];
+  # from https://wiki.nixos.org/wiki/Jellyfin
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-ocl # generic OpenCL support, for all processors
+      # For newer processors (Broadwell and higher, ca. 2014), use this paired with `LIBVA_DRIVER_NAME=iHD`:
+      intel-media-driver
+      intel-compute-runtime
+      vpl-gpu-rt
+    ];
+  };
   services.jellyseerr.enable = true; # 5055
 
   services.immich.enable = true; # 2283
