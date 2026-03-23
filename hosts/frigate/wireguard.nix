6db496899e609de378693669ca3db6d9163b4fb5
Mattys Gervais <mattysgervais@gmail.com>
2026-03-23 14:01:02 -0400

    config persistent

--- hosts/frigate/wireguard.nix
+++ hosts/frigate/wireguard.nix
@@ -3,6 +3,7 @@
   sops.secrets.frig-wg-priv = { };
 
   networking.wg-quick.interfaces.wg0 = {
+    autostart = false;
     address = [ "10.100.0.3/24" ];
     dns = [ "192.168.0.15" ];
     privateKeyFile = config.sops.secrets.frig-wg-priv.path;
@@ -10,8 +11,9 @@
     peers = [
       {
         publicKey = "R9rxBPbIhcPyBMeFfe+dH86cdYMnZIWqThp6piR4z2A=";
-        allowedIPs = [ "0.0.0.0/0" ];
+        allowedIPs = [ "192.168.0.0/24" ];
         endpoint = "vpn.mattysgervais.com:51820";
+        persistentKeepalive = 25;
       }
     ];
   };
