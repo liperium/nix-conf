1e499cc2fe6747db1cdad3c029e0644ef7aeaf29
Mattys Gervais <mattysgervais@gmail.com>
2026-06-14 01:07:59 -0400

    hm version issue

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
