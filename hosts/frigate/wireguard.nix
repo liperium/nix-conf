48ea5f39b6b6b8371c6cc15cc46eab2be2e51265
Mattys Gervais <mattysgervais@gmail.com>
2026-07-27 15:17:33 -0400

    wg config

--- hosts/frigate/wireguard.nix
+++ hosts/frigate/wireguard.nix
@@ -140,13 +140,13 @@ in
   networking.wg-quick.interfaces.wg0 = {
     autostart = false;
     address = [ "10.100.0.3/24" ];
-    dns = [ "192.168.1.15" ];
+    dns = [ "192.168.1.10" ];
     privateKeyFile = config.sops.secrets.frig-wg-priv.path;
 
     peers = [
       {
         publicKey = "R9rxBPbIhcPyBMeFfe+dH86cdYMnZIWqThp6piR4z2A=";
-        allowedIPs = [ "192.168.0.0/24" ];
+        allowedIPs = [ "192.168.1.0/24" ];
         endpoint = "vpn.mattysgervais.com:51820";
         persistentKeepalive = 25;
       }
